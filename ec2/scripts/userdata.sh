a6703846194b8ec52959bceec6cc580a116a2ebc
Pablo Bellido <bellido.peru26@gmail.com>
2024-04-29 13:15:33 -0500

    refactor: update init script

--- ec2/scripts/userdata.sh
+++ ec2/scripts/userdata.sh
@@ -2,7 +2,7 @@
 # Install docker and docker-compose
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -14,7 +14,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Add the current user to the docker group
-sudo usermod -aG docker $USER
+# Add the ubuntu user to the docker group
+sudo usermod -aG docker ubuntu
